9b2a17c5e23bc620c1b2a7d5e8d1757bd2fd3169
Valeryia <vmiron@exadel.com>
2026-03-08 20:34:16 +0400

    feat: add execCommand

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,26 @@
+import { spawn } from 'node:child_process';
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.error('No command provided');
+    process.exit(1);
+  }
+
+  const [command, ...commandArgs] = args.join(' ').split(' ');
+
+  const child = spawn(command, commandArgs, {
+    stdio: ['inherit', 'pipe', 'pipe'], 
+    env: process.env                     
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on('close', (code) => {
+    process.exit(code);
+  });
 };
 
 execCommand();
